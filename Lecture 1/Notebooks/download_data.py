4111c53680ac4c9c5fb185b40c20388f984bf2bc
bilgesipal <35695612+bilgesipal@users.noreply.github.com>
2025-02-14 13:24:47 +0300

    # First Lecture

---
+++ Lecture 1/Notebooks/download_data.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import fetch_20newsgroups
+import pickle
+
+
+if __name__ == "__main__":
+    newsgroups_train = fetch_20newsgroups(subset='train')
+    cats = ['alt.atheism', 'sci.space']
+    newsgroups_train = fetch_20newsgroups(subset='train', categories=cats)
+    df_data=newsgroups_train.data
+    print(len(df_data))
+    with open('df_data.pkl', "wb") as f:
+        pickle.dump(df_data, f)
+    label = df_target = newsgroups_train.target[:len(df_data)]
+    with open('lable.pkl', "wb") as f:
+        pickle.dump(label, f)
